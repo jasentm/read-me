68b72d19fea799876712cf18cc183cbf97650ea3
Jasen <jasen.miyamoto@gmail.com>
2024-05-14 10:46:24 -0400

    created front end routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Deck from './pages/Deck'
+import LessonList from './pages/LessonList';
+import Lesson from './pages/Lesson';
+import Profile from './pages/Profile';
+import Readings from './pages/Readings';
+import Signin from './pages/Signin';
+import SavedReading from './pages/SavedReading';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // State hook to manage user state
+  const [user, setUser] = useState(null)
+
+  // Effect hook to fetch user data on component mount
+  useEffect(() => {
+    fetch('http://localhost:8080//authenticate-session')
+    .then((res) => {
+      if (res.ok){
+        return res.json() // Parse JSON data if response is OK
+      }else{
+        console.error('user not found') // Log error if user not found
+      }
+    })
+    .then(data => setUser(data)) // Update user state with fetched data
+  }, [])
+
+  // Function to update user state
+  const updateUser = (user) => {
+    setUser(user)
+  } 
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='app'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/profile/:id' element={<Profile/>} />
+          <Route path='/deck' element={<Deck/>}/>
+          <Route path='/lesson-list' element={<LessonList/>}/>
+          <Route path='/lesson/:id' element={<Lesson/>}/>
+          <Route path='/readings' element={<Readings/>}/>
+          <Route path='/readings/:id' element={<SavedReading/>}/>
+          <Route path='/signin' element={<Signin/>}/>
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ client/src/pages/LessonList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function LessonList() {
+  return (
+    <div>LessonList</div>
+  )
+}
